0fb92bec816931befb50f88d51f1e4e6d1ce8414
rasgarov <rashid.asgarov.21@gmail.com>
2023-03-17 13:48:10 +0400

    s

--- training_project/artoStudy/assets/js/by_me.js
+++
@@ -1,8 +0,0 @@
-// Rashid Start
-const myForm = document.querySelector("#contact-form");
-
-myForm.addEventListener("submit",(e)=>{
-    alert("Mesajınız uğurla göndərildi!");
-    myForm.reset();
-})
-// Rashid End
